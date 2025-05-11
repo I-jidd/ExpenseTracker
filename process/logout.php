377fc88897502ccdff51301b6aa32e452a3d3a53
I-jidd <jade.notsopersonal@gmail.com>
2025-05-11 18:36:22 +0800

    Refactor logout process to ensure remember-me cookie is cleared and database token is updated

--- process/logout.php
+++ process/logout.php
@@ -1,8 +1,22 @@
 <?php
     session_start();
+    require_once '../includes/connection.php';
 
     $_SESSION = array();
 
     session_destroy();
+    
+    // Clear remember me cookie
+    if (isset($_COOKIE['remember_me'])){
+        // clear the database token
+        $token = $_COOKIE['remember_me'];
+        $stmt = $conn->prepare("UPDATE users SET remember_token = NULL WHERE remember_token = ?");
+        $stmt->bind_param("s", $token);
+        $stmt -> execute();
+
+        setcookie('remember_me', '', time() - 3600, '/');
+        
+
+    }
     header('Location: ../public/login-page.php?logout=success');
     exit();
